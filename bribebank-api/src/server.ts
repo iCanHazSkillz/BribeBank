f934b9d6d4b745908ec7336cb84fd7c6b21a1093
icanhazskillz <m.martel02@gmail.com>
2025-12-26 22:35:28 -0700

    Add TWA support: Bubblewrap implementation

--- bribebank-api/src/server.ts
+++ bribebank-api/src/server.ts
@@ -45,7 +45,7 @@ app.get('/.well-known/assetlinks.json', (_req, res) => {
       "namespace": "android_app",
       "package_name": "com.bribebank.app",
       "sha256_cert_fingerprints": [
-        "PLACEHOLDER_WILL_BE_GENERATED_BY_BUBBLEWRAP"
+        "60:57:D3:9F:DD:10:A0:6A:D2:44:7A:C8:15:E5:EF:44:91:67:DB:07:AA:81:D9:DF:70:62:AF:E7:C9:1A:6B:0B"
       ]
     }
   }]);
